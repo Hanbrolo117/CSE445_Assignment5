47cee9000044db86285d206a1e329f03fa443de6
Kyle Ferguson <hanbrolo117@gmail.com>
2016-11-19 00:34:55 -0700

    added stuff

---
+++ CSE445_Assignment5/Staff/Staff.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Staff.aspx.cs" Inherits="CSE445_Assignment5.Staff.Staff" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:Label ID="Label1" runat="server" Text="Staff user list:"></asp:Label>
+        <br />
+        <asp:TextBox ID="txtStaffs" runat="server" Height="146px" ReadOnly="True" TextMode="MultiLine" Width="116px"></asp:TextBox>
+&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="btnAddStaff" runat="server" OnClick="btnAddStaff_Click" Text="Add New Staff" />
+        <asp:Label ID="Label2" runat="server" Text="(For admin use only)"></asp:Label>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ CSE445_Assignment5/StaffLogin.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+using System.Xml;
+using CryptionLibrary;
+
+namespace CSE445_Assignment5
+{
+    public partial class StaffLogin : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            this.signIn.subscribeToLoginButton(this.LoginHandler);
+            this.signIn.subscribeToRegisterButton(this.RegisterHandler);
+        }
+
+        public void LoginHandler(string username, string password, EventArgs e)
+        {
+            /*string fLocation = Path.Combine(HttpRuntime.AppDomainAppPath, @"App_Data\Staff.xml");
+            XmlNode node = XMLProccess.findUser(fLocation, username);
+            if (node == null)
+            {
+                alert.Text = "User does not exist";
+            }
+            else if (string.Compare(node["password"].InnerText, password) != 0)
+            {
+                alert.Text = "wrong Password";
+            }
+            else
+                alert.Text = "Login Sucess!";  */
+        }
+
+        public void RegisterHandler(string username, string password, EventArgs e)
+        {
+            this.alert.Text = string.Format("Registering! username: {0} | password: {1}", username, password);
+        }
+    }
+}
