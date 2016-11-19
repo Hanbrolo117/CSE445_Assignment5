f4128237eb13a9e2f7c6f14017e9e726d7c607df
Kyle Ferguson <hanbrolo117@gmail.com>
2016-11-18 22:43:45 -0700

    Add project files.

---
+++ CSE445_Assignment5/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace CSE445_Assignment5.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ CSE445_Assignment5/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CSE445_Assignment5.Startup))]
+namespace CSE445_Assignment5
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
