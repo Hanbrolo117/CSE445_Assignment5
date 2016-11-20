682afed48f654b5a48fca4ab79aedcf500214471
Khal <krast214@gmail.com>
2016-11-20 12:18:17 -0700

    Implemented memberregister and memberlogin fully.

--- CSE445_Assignment5/GUI/Member/MemberRegister.aspx.cs
+++ CSE445_Assignment5/GUI/Member/MemberRegister.aspx.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using TeamLibrary;
 
 namespace CSE445_Assignment5.GUI.Member
 {
@@ -22,16 +24,23 @@ namespace CSE445_Assignment5.GUI.Member
         public void RegisterHandler(string username, string password, string confirmPassword, EventArgs e) {
             if (password.Equals(confirmPassword))
             {
-                string encryptedPassword = TeamLibrary.Crypto.encryption(password);
+                string encryptedPassword = Crypto.encryption(password);
                 //Insert New User:
-                //Create A login Cookie for managing the session:
-                HttpCookie loginCookie = new HttpCookie("member");//Create Staff Cookie.
-                loginCookie["username"] = username;//Set Staff Cookie username.
-                loginCookie.Expires = DateTime.Now.AddMonths(1);//Set Cookie Expiration for 1 month.
-                Response.Cookies.Add(loginCookie);
+                string fLocation = Path.Combine(HttpRuntime.AppDomainAppPath, @"App_Data\Member.xml");
 
-                alert.Text = "Login Sucess!";
-                Response.Redirect("Member");
+                if (XMLProccess.findUser(fLocation, username) == null)
+                {
+                    XMLProccess.addUser(fLocation, username, encryptedPassword);
+                    //Create A login Cookie for managing the session:
+                    HttpCookie loginCookie = new HttpCookie("member");//Create Staff Cookie.
+                    loginCookie["username"] = username;//Set Staff Cookie username.
+                    loginCookie.Expires = DateTime.Now.AddMonths(1);//Set Cookie Expiration for 1 month.
+                    Response.Cookies.Add(loginCookie);
+                    alert.Text = "Login Sucess!";
+                    Response.Redirect("Member");
+                }
+                else
+                    alert.Text = "Username already exists!";
             }
             else {
                 alert.Text = "Passwords did not match!";
